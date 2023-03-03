1ace95dc52eb51814e0d4591177723b4af64d408
SalahELDineAlAyoubi <salah.a.al.ayoubi@gmail.com>
2023-03-03 03:15:17 +0200

    update findChat

--- Controllers/ChatController.js
+++ Controllers/ChatController.js
@@ -28,8 +28,27 @@ exports.findChat = async (req, res) => {
     const chat = await ChatModel.findOne({
       members: { $all: [req.params.firstId, req.params.secondId] },
     });
-    res.status(200).json(chat);
+   if (chat) {
+     res.status(200).json(chat);
+   } else {
+     const newChat = new ChatModel({
+       members: [req.params.firstId, req.params.secondId],
+     });
+     const result = await newChat.save();
+     res.status(200).json(result);
+   }
   } catch (error) {
     res.status(500).json(error);
   }
 };
+ /*exports.findChat = async (req, res) => {
+  try {
+    const chat = await ChatModel.findOne({
+      members: { $all: [req.params.firstId, req.params.secondId] },
+    });
+    res.status(200).json(chat);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+}; 
+*/
